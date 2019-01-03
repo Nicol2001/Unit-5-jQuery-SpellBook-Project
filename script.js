a024c0e5fd65457fd389a255b27620bc8dbacede
Nicol2001 <43443758+Nicol2001@users.noreply.github.com>
2019-01-03 09:47:08 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+$("#birdbox").click(function(){
+ $("#birdbox").fadeOut();
+});
+
+
+
+//Spell #2
+
+$("#dog").click(function(){
+ $("#dog").slideUp();
+});
+
+
+
+//Spell #3
+
+$("#unicorn").click(function(){
+ $("#unicorn").toggle();
+});
+
+
+
+
